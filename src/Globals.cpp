74c05d4bb15746c627e54db666f7fb0fb7d96103
Pablo Ventura <pablogventura@gmail.com>
2026-03-05 17:22:09 -0300

    chore(version): update version number from 0.99 to 1.0 in Globals.cpp

--- src/Globals.cpp
+++ src/Globals.cpp
@@ -347,7 +347,7 @@ IPAddress gateway;
 IPAddress subnet;
 IPAddress primaryDNS;
 IPAddress secondaryDNS;
-const char *VERSION = "0.99";
+const char *VERSION = "1.0";
 String MQTT_HOST = "";
 uint16_t MQTT_PORT = 1883;
 String MQTT_USER;
